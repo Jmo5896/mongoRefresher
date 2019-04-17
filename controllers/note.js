629a044380ade54eeb34a09e11c8be9ad907b03c
Jmo5896 <jmo5896@moore-clan.net>
2019-04-17 13:16:14 -0600

    note controller is done, moving on to routes

---
+++ controllers/note.js
@@ -0,0 +1,19 @@
+let db = require('../models');
+
+module.exports = {
+    find: (req, res) => {
+        db.Note
+            .find({ _headlineId: req.params.id })
+            .then(dbNote => res.json(dbNote));
+    },
+    create: (req, res) => {
+        db.Note
+            .create(req.body)
+            .then(dbNote => res.json(dbNote));
+    },
+    delete: (req, res) => {
+        db.Note
+            .remove({ _id: req.params.id })
+            .then(dbNote => res.json(dbNote));
+    }
+};
